72c1e22ad7537f23e27abceea3109757c8925f62
Yuliya Filimonova <yuliya_nova@icloud.com>
2022-08-03 14:36:46 +0300

    Initial Commit

---
+++ AboutRick&Morty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutRick&Morty
+//
+//  Created by Юлия Филимонова on 03.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
